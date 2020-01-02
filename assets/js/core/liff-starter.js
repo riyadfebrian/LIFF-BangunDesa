f35347b6f73ef6df1d7c369bbd36ccb2e9bd3f9a
Riyad Febrian <riyadfebrian@gmail.com>
2020-01-02 17:11:17 +0700

    added promise handler

--- assets/js/core/liff-starter.js
+++ assets/js/core/liff-starter.js
@@ -64,6 +64,16 @@ function initializeApp() {
         // document.getElementById("profile-img-placeholder").src = profile.pictureUrl
 
         console.log("Profile => " + liff.getProfile())
+
+        liff.getProfile().then(profile => {
+            console.log("Display name = " + profile.displayName)
+            console.log("userId" = + profile.userId)
+            console.log("pictureUrl" = + profile.pictureUrl)
+        }).catch((err) => {
+            console.log('error', err);
+        });
+
+
     } else {
         liff.login()
     }
