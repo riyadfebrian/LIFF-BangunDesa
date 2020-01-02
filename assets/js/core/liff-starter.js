6a60b4c1fc497c9752f0d2fc4dd97603ebc9ed8a
Riyad Febrian <riyadfebrian@gmail.com>
2020-01-02 18:09:08 +0700

    naha te bisa logout di line browser

--- assets/js/core/liff-starter.js
+++ assets/js/core/liff-starter.js
@@ -96,6 +96,7 @@ function logout() {
 
 $(document).ready(function() {
     document.getElementById('logoutButton').addEventListener('click', function() {
+            liff.logout();
             liff.closeWindow();
     });
 
